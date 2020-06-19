e40b6a5b1aab090eea677bb9b871ea6ccbe9e4f8
ssj <850579014@qq.com>
2020-06-19 12:18:25 +0800

    1.增加验证次数 2.增加倒计时

--- app/src/main/java/com/shengshijie/test/MainActivity.kt
+++ app/src/main/java/com/shengshijie/test/MainActivity.kt
@@ -3,6 +3,7 @@ package com.shengshijie.test
 import android.os.Bundle
 import android.util.Log
 import android.widget.Button
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.shengshijie.pwdinput.view.PwdInputDialog
 
@@ -12,17 +13,27 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         val button = findViewById<Button>(R.id.button)
         button.setOnClickListener {
+            var verifyTime = 3
             PwdInputDialog.newInstance(this)
                     .setTitle("本次交易需要辅助验证")
                     .setMessage("请验证用户编号后6位")
                     .setPasswordLength(4)
                     .setUser("用户:张*")
-                    .setOnDismiss { Log.e("DDD","AAA") }
+                    .setTimeout(5)
+                    .setOnDismissListener { Log.e("DDD", "setOnDismissListener") }
+                    .setOnTimeoutListener { Log.e("DDD", "setOnTimeoutListener") }
                     .setOnPwdInputComplete { text, dialog ->
                         if ("1234" == text) {
                             dialog.dismiss()
+                            Toast.makeText(this, "验证成功", Toast.LENGTH_SHORT).show()
                         } else {
-                            dialog.setResult("验证失败")
+                            if (verifyTime <= 1) {
+                                dialog.dismiss()
+                                Toast.makeText(this, "验证次数已达上限", Toast.LENGTH_SHORT).show()
+                                return@setOnPwdInputComplete
+                            }
+                            verifyTime--
+                            dialog.setResult("验证失败,剩余验证次数:$verifyTime")
                         }
                     }.show()
         }
--- pwdinput/src/main/java/com/shengshijie/pwdinput/view/PwdInputDialog.kt
+++ pwdinput/src/main/java/com/shengshijie/pwdinput/view/PwdInputDialog.kt
@@ -1,9 +1,11 @@
 package com.shengshijie.pwdinput.view
 
 import android.os.Bundle
+import android.os.SystemClock
 import android.text.Editable
 import android.text.TextWatcher
 import android.view.*
+import android.widget.Chronometer
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.DialogFragment
@@ -11,12 +13,15 @@ import com.shengshijie.pwdinput.R
 
 class PwdInputDialog(private var activity: AppCompatActivity) : DialogFragment() {
     private var onPwdInputComplete: (String, PwdInputDialog) -> Unit? = { _, _ -> }
-    private var onDismiss: () -> Unit? = {}
+    private var onDismissListener: () -> Unit? = {}
+    private var onTimeoutListener: () -> Unit? = {}
     private var title: String = ""
     private var message: String = ""
     private var user: String = ""
     private var length = 6
     private var tvResult: TextView? = null
+    private var tvTimeout: Chronometer? = null
+    private var timeout: Int = 60
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         dialog?.requestWindowFeature(Window.FEATURE_NO_TITLE)
@@ -32,6 +37,14 @@ class PwdInputDialog(private var activity: AppCompatActivity) : DialogFragment()
         pwdInputView.passwordLength = length
         val tvTitle: TextView = view.findViewById(R.id.tv_title)
         tvTitle.text = title
+        tvTimeout = view.findViewById(R.id.tv_timeout)
+        tvTimeout?.format = "%s"
+        tvTimeout?.base = SystemClock.elapsedRealtime() + timeout * 1000
+        tvTimeout?.start()
+        tvTimeout?.onChronometerTickListener = Chronometer.OnChronometerTickListener {
+            tvTimeout?.text = "${tvTimeout?.text?.substring(1)}"
+            if (SystemClock.elapsedRealtime() - (tvTimeout?.base?:0L) > 0) onTimeout()
+        }
         val tvMessage: TextView = view.findViewById(R.id.tv_message)
         tvMessage.text = message
         val tvUser: TextView = view.findViewById(R.id.tv_user)
@@ -39,7 +52,7 @@ class PwdInputDialog(private var activity: AppCompatActivity) : DialogFragment()
         tvResult = view.findViewById(R.id.tv_result)
         keyboardView.bindTextView(pwdInputView) {
             dismiss()
-            onDismiss()
+            onDismissListener()
         }
         pwdInputView.addTextChangedListener(object : TextWatcher {
             override fun beforeTextChanged(charSequence: CharSequence, i: Int, i1: Int, i2: Int) {}
@@ -54,6 +67,12 @@ class PwdInputDialog(private var activity: AppCompatActivity) : DialogFragment()
         })
     }
 
+    private fun onTimeout() {
+        tvTimeout?.stop()
+        dismiss()
+        onTimeoutListener()
+    }
+
     override fun onResume() {
         val window: Window? = dialog?.window
         window?.setLayout(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT)
@@ -66,8 +85,18 @@ class PwdInputDialog(private var activity: AppCompatActivity) : DialogFragment()
         show(activity.supportFragmentManager, "")
     }
 
-    fun setOnDismiss(onDismiss: () -> Unit): PwdInputDialog {
-        this.onDismiss = onDismiss
+    fun setTimeout(timeout: Int): PwdInputDialog {
+        this.timeout = timeout
+        return this
+    }
+
+    fun setOnTimeoutListener(onTimeoutListener: () -> Unit): PwdInputDialog {
+        this.onTimeoutListener = onTimeoutListener
+        return this
+    }
+
+    fun setOnDismissListener(onDismissListener: () -> Unit): PwdInputDialog {
+        this.onDismissListener = onDismissListener
         return this
     }
 
